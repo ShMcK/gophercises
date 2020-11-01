d8bd0d8818402007019608c7acf5399e53b4472c
shmck <shawn.j.mckay@gmail.com>
2020-11-01 10:20:35 -0800

    add deleteItem

--- rest-api/main.go
+++ rest-api/main.go
@@ -40,12 +40,29 @@ func createItem(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(item)
 }
 
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for i, item := range inventory {
+		if item.ID == id {
+			inventory = append(inventory[:i], inventory[i+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(inventory)
+}
+
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage).Methods("GET")
 	router.HandleFunc("/inventory", getInventory).Methods("GET")
 	router.HandleFunc("/inventory", createItem).Methods("POST")
+	router.HandleFunc("/inventory/{id}", deleteItem).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
