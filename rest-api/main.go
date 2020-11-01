b693df661a4d717c53d10e2daa7d926835c0f08a
shmck <shawn.j.mckay@gmail.com>
2020-11-01 10:02:16 -0800

    add json endpoint

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,18 +9,42 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Item an inventory item
+type Item struct {
+	ID    string  `json:"ID"`
+	Name  string  `json:"Name"`
+	Desc  string  `json:"Desc"`
+	Price float64 `json:"Price"`
+}
+
+var inventory []Item
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
+	fmt.Fprintf(w, "<h1>Home Page</h1>")
+}
+
+func getInventory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("getInventory")
+
+	json.NewEncoder(w).Encode(inventory)
 }
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/inventory", getInventory).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func main() {
+	inventory = append(inventory, Item{
+		ID:    "1",
+		Name:  "Pumpkin",
+		Desc:  "A pre-jack-o-lantern",
+		Price: 5.99,
+	})
 	handleRequests()
 }
